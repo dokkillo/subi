5b20b93f839e7d365a80f3a71e51d8e09a4bf8cb
gabriel.costa.externo <gabriel.costa.externo@axa-assistance.es>
2020-12-09 10:07:55 +0100

    added certspotter

--- subi.sh
+++ subi.sh
@@ -3,6 +3,8 @@ curl -s "https://dns.bufferover.run/dns?q=.${1}" | jq -r .FDNS_A[] |cut -d "," -
 curl -s "https://riddler.io/search/exportcsv?q=pld:${1}" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u >>temp.txt
 curl -s "http://web.archive.org/cdx/search/cdx?url=*.${1}/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | cut -d ":" -f1 | sort -u >> temp.txt
 curl -s 'https://securitytrails.com/list/apex_domain/${1}' | grep -Po '((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+' | grep '.${1}' | sort -u >> temp.txt
+curl -s "https://api.certspotter.com/v1/issuances?domain=${1}&include_subdomains=True&expand=dns_names" | jq -r .[].dns_names |  grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+"
+
 sort -u temp.txt | tee result.txt
 
 
