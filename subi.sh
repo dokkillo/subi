42436b9c23bdbf4ab1ea6c5e71a0c5f24756a3da
gabriel.costa.externo <gabriel.costa.externo@axa-assistance.es>
2020-12-09 09:52:09 +0100

    init

---
+++ subi.sh
@@ -0,0 +1,9 @@
+curl -s "https://crt.sh/?q=%25.${1}&output=json" | jq -r .[].name_value | sed 's/\*\.//g' | sort -u | tee temp.txt 
+curl -s "https://dns.bufferover.run/dns?q=.${1}" | jq -r .FDNS_A[] |cut -d "," -f2|sort -u >> temp.txt
+curl -s "https://riddler.io/search/exportcsv?q=pld:${1}" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u >>temp.txt
+curl -s "http://web.archive.org/cdx/search/cdx?url=*.${1}/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | cut -d ":" -f1 | sort -u >> temp.txt
+curl -s 'https://securitytrails.com/list/apex_domain/${1}' | grep -Po '((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+' | grep '.${1}' | sort -u >> temp.txt
+cat temp.txt | sort -u | tee result.txt
+echo "-----------------------"
+cat result.txt
+wc -l result.txt
